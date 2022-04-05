cbe83e3fc965fcd8b45dc5c05504e810588d6b8f
DONAT-FILLIOD Nathan <nathan.donat-filliod@epitech.eu>
2022-04-05 20:14:29 +0200

    [1.1]
    [ADD] + Car move and droit dans l'mur

--- src/main.c
+++ src/main.c
@@ -7,7 +7,35 @@
 
 #include "../includes/ai.h"
 
+void launch_car(void)
+{
+    printf("START_SIMULATION\n");
+    printf("CAR_FORWARD:0.6\n");
+}
+
+void game_loop(void)
+{
+    char *buffer;
+    struct stat st;
+
+    while (1) {
+        fstat(STDIN_FILENO, &st);
+        buffer = malloc(sizeof(char) * st.st_size);
+        read(STDIN_FILENO, buffer, st.st_size);
+        printf("GET_INFO_LIDAR\n");
+    }
+}
+
+void stop_car(void)
+{
+    printf("CAR_FORWARD:0\n");
+    printf("STOP_SIMULATION\n");
+}
+
 int main(int argc, char **argv)
 {
+    launch_car();
+    game_loop();
+    stop_car();
     return (0);
 }
